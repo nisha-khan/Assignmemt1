576e497a4c23ada2d0ca77dbc20853030f80110c
Nisha Khan <nishakhan.work@gmail.com>
2023-05-29 11:50:39 +0500

    Commited

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,30 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "What is the file extension for a JavaScript file?",
+            "Which one is NOT the programming language?",
+            "What is the binary representation of the decimal number 5?",
+            "Which company owns the Apple?",
+            "What is the largest unit of digital storage?"
+    };
+
+    public static String choices[][] = {
+            {".js",",html",".css",".py"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"101","100","111","110"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"MB","KB","GB","TB"}
+
+    };
+
+    public static String correctAnswers[] = {
+            ".js",
+            "Notepad",
+            "101",
+            "Apple",
+            "TB"
+    };
+
+}
